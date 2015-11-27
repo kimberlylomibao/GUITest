5d7b61607cd05f3da578489a793047fc221444d2
Kimberly Lomibao <lomibaokimberly@gmail.com>
2015-11-27 10:34:09 +0800

    Initial Commit

---
+++ CountFactor.java
@@ -0,0 +1,28 @@
+/**
+ * @(#)CountFactor.java
+ *
+ *
+ * @author 
+ * @version 1.00 2015/11/9
+ */
+
+import java.util.Scanner;
+
+public class CountFactor {	
+    public static void main(String[] args) {
+        int count = 0; 
+    	int N = 0;
+    	
+    	Scanner kbd = new Scanner(System.in);
+    	System.out.print("Input: "); // change label
+    	N = kbd.nextInt();
+    	
+    	for (int i = 1; i < Math.sqrt(N); i++) {
+    		if (N % i == 0) {
+    			count += 2;
+    		}
+    	}
+    	
+//    	System.out.print("Number of Factors: " +count); // add comment
+    }     
+}
